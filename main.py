b2132ae6ba30f3536571f6120c1f29ac13339c7c
lucas <lucas.p.jensen10@gmail.com>
2021-12-15 14:55:14 -0800

    First working version.

--- main.py
+++ main.py
@@ -4,5 +4,97 @@ The main program for transferring data between Solv and ALERT iis
 Last updated: 12/15/2021
 """
 
-from patient_immunization import Patient
+import csv
+from patient_immunization import Patient, Immunization
 
+month = "TEST_MONTH"
+solv_file = "data/Solv_data/solv_patient_report_Outreach Vaccine Clinic_O_20211102_20211214_814ea322-5de2-11ec-9ba4-32ce41331b1f.csv"
+patient_file = f"data/Patient/{month}_patients.csv"
+immunization_file = f"data/Immunization/{month}_immunizations.csv"
+
+
+def gather_date(data_file):
+    """
+    Creates a list of patient objects based on a csv file from Solv
+    :param data_file: name of the data file from Solv
+    :return: list of patient objects
+    """
+    with open(data_file, 'r') as infile:
+        reader = csv.reader(infile)
+        header_row = next(reader)
+        patients = []
+
+        # Set important indices or values from Solv
+        vaccination_date = header_row.index('Date')
+        first_name = header_row.index('Patient First Name')
+        middle_name = ""
+        last_name = header_row.index('Patient Last Name')
+        birth_date = header_row.index('Date Of Birth')
+        race = header_row.index('Patient Race')
+        ethnicity_index = header_row.index('Patient Ethnicity')
+        sex_index = header_row.index('Birth Sex')
+        giver_index = header_row.index('Name of Vaccine Administrator (First/Last)')
+        lot_index = header_row.index('Lot#')
+        arm = header_row.index('Which arm is receiving vaccine?')
+        brand = header_row.index('Vaccine Brand')
+        # insert code for maiden name not existing as a column
+        mother_maiden_index = header_row.index("Please enter your Mother's Maiden name")
+
+        for i, row in enumerate(reader):
+            if row[lot_index]:
+                immunization = Immunization(row[brand], row[vaccination_date], row[arm], row[lot_index],
+                                            row[giver_index])
+                patient = Patient(i + 1, row[first_name], middle_name, row[last_name], row[birth_date],
+                                  row[mother_maiden_index], row[sex_index], row[race], row[ethnicity_index],
+                                  immunization)
+                patient.set_race_info()
+                patients.append(patient)
+
+    return patients
+
+
+def make_patient_file(patients, filename):
+    """
+    Makes a csv file of patient info
+    :param patients: list of patient objects
+    :param filename: the filename for the patient file, previously defined
+    :return: nothing
+    """
+    with open(filename, 'w') as outfile:
+        # file header
+        outfile.write(
+            "Blank,Record Identifier,Patient Status,First Name,Middle Name,Last Name,Name Suffix,Birth Date,Death Date,Mother's First Name,Mother's Maiden Last Name,Mother's HBsAG Status,Sex (Gender),American Indian or Alaska Native,Asian,Native Hawaiian or Other Pacific Islander,Black or African-American,White,Other Race,Ethnicity,Social Security Number,Contact Allowed,Patient ID,Medicaid ID,Responsible Party First Name,Responsible Party Middle Name,Responsible Party Last Name,Responsible Party Relationship,Street Address Line,Other Address Line,PO Box Route Line,City,State,Zip Code,County,Phone,Sending Organization\n")
+        for patient in patients:
+            # all rows start with a blank entry. No idea why. It's in the ALERT iis documentation
+            blank = ""
+            outfile.write(
+                f"{blank},{patient.get_record()},{patient.get_status()},{patient.get_first()},{patient.get_middle()},{patient.get_last()},{patient.get_suffix()},{patient.get_birth_date()},{patient.get_death_date()},{patient.get_mother_first()},{patient.get_mother_maiden()},{patient.get_mother_HBsAG()},{patient.get_sex()},{patient.get_native_american()},{patient.get_asian()},{patient.get_native_hawaiian()},{patient.get_black()},{patient.get_white()},{patient.get_other()},{patient.get_ethnicity()},{patient.get_ssn()},{patient.get_contact()},{patient.get_patient_id()},{patient.get_medicaid_id()},{patient.get_rp_first()},{patient.get_rp_middle()},{patient.get_rp_last()},{patient.get_rp_relation()},{patient.get_street_address()},{patient.get_other_address()},{patient.get_po_box()},{patient.get_city()},{patient.get_state()},{patient.get_zip()},{patient.get_county()},{patient.get_phone()},{patient.get_sending_org()}\n")
+
+
+def make_immunization_file(patients, filename):
+    """
+    Makes a csv file of immunization info
+    :param patients:
+    :param filename:
+    :return:
+    """
+    with open(filename, 'w') as outfile:
+        # file header
+        outfile.write(
+            "Blank,Record Identifier,NDC Code,Trade Name,CPT Code,CVX Code,Vaccine Group,Vaccination Date,Administration Route Code,Body Site Code,Reaction Code,Manufacturer Code,Immunization Information Source,Lot Number,Provider Name,Administered By Name,Sending Organization,Vaccine Eligibility\n")
+        for patient in patients:
+            immunization = patient.get_immunization()
+            # all rows start with a blank entry. No idea why. It's in the ALERT iis documentation
+            blank = ""
+            outfile.write(
+                f"{blank},{patient.get_record()},{immunization.get_ndc()},{immunization.get_trade_name()},{immunization.get_cpt()},{immunization.get_cvx()},{immunization.get_vaccine_group()},{immunization.get_vaccination_date()},{immunization.get_route()},{immunization.get_body_site()},{immunization.get_reaction()},{immunization.get_manufacturer_code()},{immunization.get_info_source()},{immunization.get_lot_number()},{immunization.get_provider_name()},{immunization.get_giver()},{immunization.get_sending_org()},{immunization.get_eligibility()}\n")
+
+
+def main():
+    patients = gather_date(solv_file)
+    make_patient_file(patients, patient_file)
+    make_immunization_file(patients, immunization_file)
+
+
+if __name__ == "__main__":
+    main()
--- patient_immunization.py
+++ patient_immunization.py
@@ -4,46 +4,135 @@ Patient class and Immunization class for main.py of the Solv -> ALERT program
 Last updated: 12/15/2021
 """
 
+
+def format_date(date):
+    """
+    Param: date: YYYY-MM-DD or MM/DD/YYYY or M/DD/YYYY
+    return: MMDDYYYY as string
+    """
+
+    new_list = []
+
+    if date[4] == '-':
+
+        new_list.append(date[5])
+        new_list.append(date[6])
+        new_list.append(date[8])
+        new_list.append(date[9])
+        new_list.append(date[0])
+        new_list.append(date[1])
+        new_list.append(date[2])
+        new_list.append(date[3])
+    else:
+        for char in date:
+            if char != '/':
+                new_list.append(char)
+
+    new_date = ''.join(new_list)
+
+    return new_date
+
+
+def format_body_site(arm_str):
+    """
+    formats the site of the vaccine injection
+    :param arm_str: "Left Deltoid" or "Right Deltoid"
+    :return: "LD" or "RD"
+    """
+    if "right" in arm_str.lower():
+        return "RD"
+    elif "left" in arm_str.lower():
+        return "LD"
+    else:
+        return ""
+
+
+def format_sex(sex):
+    """
+    Reformats a patient's gender to match ALERT iis guidelines
+    :param sex: "Male" or "Female"
+    :return: "M" or "F"
+    """
+    if sex == "Male":
+        return "M"
+    elif sex == "Female":
+        return "F"
+    else:
+        return "U"
+
+
+def format_ethnicity(ethnicity):
+    """
+    Reformats a patient's ethnicity to match ALERT iis guidelines
+    :param ethnicity: a string of ethnicity
+    :return: "NH" or "H"
+    """
+    if ethnicity == "Not Hispanic/Latino":
+        return "NH"
+    elif ethnicity == "Hispanic/Latino":
+        return "H"
+    else:
+        return ""
+
+
 class Patient:
     """Represents an immunization given, intended for COVID-19 vaccines"""
-    def __init__(self, record, first, middle, last, birth_date, maiden, sex, native_american, asian, native_hawaiian, black,
-                 white, other_race, ethnicity):
+
+    def __init__(self, record, first, middle, last, birth_date, maiden, sex, race, ethnicity, immunization_obj):
+        """Initialize all data members as private"""
         self._record = record
         self._status = "A"
         self._first = first
         self._middle = middle
         self._last = last
-        self._suffix = None
+        self._suffix = ""
         self._birth_date = birth_date
-        self._death_date = None
+        self._death_date = ""
+        self._mother_first = ""
         self._mother_maiden = maiden
-        self._mother_HBsAG = None
+        self._mother_HBsAG = ""
         self._sex = sex
-        self._native_american = native_american
-        self._asian = asian
-        self._native_hawaiian = native_hawaiian
-        self._black = black
-        self._white = white
-        self._other_race = other_race
+        self._race = race
+        self._native_american = ""
+        self._asian = ""
+        self._native_hawaiian = ""
+        self._black = ""
+        self._white = ""
+        self._other_race = ""
         self._ethnicity = ethnicity
-        self._ssn = None
-        self._contact = None
-        self._patient_id = None
-        self._medicaid_id = None
-        self._rp_first = None  # responsible party first name
-        self._rp_middle = None
-        self._rp_last = None
-        self._rp_relationship = None
-        self._street_address = None
-        self._other_address = None
-        self._po_box = None
-        self._city = None
-        self._state = None
-        self._zip = None
-        self._county = None
-        self._phone = None
+        self._ssn = ""
+        self._contact = ""
+        self._patient_id = ""
+        self._medicaid_id = ""
+        self._rp_first = ""  # responsible party first name
+        self._rp_middle = ""
+        self._rp_last = ""
+        self._rp_relationship = ""
+        self._street_address = ""
+        self._other_address = ""
+        self._po_box = ""
+        self._city = ""
+        self._state = ""
+        self._zip = ""
+        self._county = ""
+        self._phone = ""
         self._sending_org = "AL3080"  # AL3080 is the organization code for White Bird Clinic
-        # self._immunization = Immunization()
+        self._immunization_obj = immunization_obj
+
+    def set_race_info(self):
+        """Uses the race parameter to set more specific details"""
+        if self._race == "White":
+            self._white = "Y"
+        elif self._race == "American Indian or Alaska Native":
+            self._native_american = "Y"
+        elif self._race == "Black or African American":
+            self._black = "Y"
+        elif self._race == "Hawaiian or Pacific Islander":
+            self._native_hawaiian = "Y"
+        elif self._race == "Asian":
+            self._asian = "Y"
+        elif self._race == "Other":
+            self._other_race = "Y"
 
     def get_record(self):
         return self._record
@@ -64,10 +153,13 @@ class Patient:
         return self._suffix
 
     def get_birth_date(self):
-        return self._birth_date
+        return format_date(self._birth_date)
 
     def get_death_date(self):
         return self._death_date
+
+    def get_mother_first(self):
+        return self._mother_first
 
     def get_mother_maiden(self):
         return self._mother_maiden
@@ -76,7 +168,7 @@ class Patient:
         return self._mother_HBsAG
 
     def get_sex(self):
-        return self._sex
+        return format_sex(self._sex)
 
     def get_native_american(self):
         return self._native_american
@@ -97,7 +189,7 @@ class Patient:
         return self._other_race
 
     def get_ethnicity(self):
-        return self._ethnicity
+        return format_ethnicity(self._ethnicity)
 
     def get_ssn(self):
         return self._ssn
@@ -119,6 +211,9 @@ class Patient:
 
     def get_rp_last(self):
         return self._rp_last
+
+    def get_rp_relation(self):
+        return self._rp_relationship
 
     def get_street_address(self):
         return self._street_address
@@ -146,3 +241,126 @@ class Patient:
 
     def get_sending_org(self):
         return self._sending_org
+
+    def get_immunization(self):
+        return self._immunization_obj
+
+
+class Immunization:
+    """Represents a COVID-19 immunization. We be used in the Patient class"""
+
+    def __init__(self, manufacturer, vaccination_date, body_site, lot_number, giver):
+        self._ndc = self._set_ndc(manufacturer)
+        self._trade_name = self._set_trade_name(manufacturer)
+        self._cpt = self._set_cpt(manufacturer)
+        self._cvx = self._set_cvx(manufacturer)
+        self._vaccine_group = "COVID-19"
+        self._vaccination_date = vaccination_date
+        self._route_code = "IM"  # intramuscular
+        self._body_site = body_site
+        self._reaction_code = ""
+        self._manufacturer_code = ""  # this code is not required and supporting documentation is incomplete
+        self._info_source = ""
+        self._lot_number = lot_number
+        self._provider_name = "White Bird Medical Clinic"
+        self._giver = giver
+        self._sending_org = "AL3080"
+        self._eligibility = "O"  # "O" is the code for "Other State Supplied"
+
+    def _set_ndc(self, manufacturer):
+        manu = manufacturer.lower()
+        if manu == "moderna":
+            return "80777-0273-10"
+        elif manu == "janssen" or manu == "j&j":
+            return "59676-0580-05"
+        elif manu == "pfizer":
+            return "59267-1000-01"
+        else:
+            print("ERROR: check manufacturer column")
+            return False
+
+    def _set_cpt(self, manufacturer):
+        manu = manufacturer.lower()
+        if manu == "moderna":
+            return "91301"
+        elif manu == "janssen" or manu == "j&j":
+            return "91303"
+        elif manu == "pfizer":
+            return "91300"
+        else:
+            print("ERROR: check manufacturer column (cpt)")
+            return False
+
+    def _set_trade_name(self, manufacturer):
+        manu = manufacturer.lower()
+        if manu == "moderna":
+            return "Moderna COVID-19 Vaccine"
+        elif manu == "janssen" or manu == "j&j":
+            return "Janssen COVID-19 Vaccine"
+        elif manu == "pfizer":
+            return "Pfizer COVID-19 Vaccine"
+        else:
+            print("ERROR: check manufacturer column (trade)")
+            print(manu)
+            return False
+
+    def _set_cvx(self, manufacturer):
+        manu = manufacturer.lower()
+        if manu == "moderna":
+            return "207"
+        elif manu == "janssen" or manu == "j&j":
+            return "212"
+        elif manu == "pfizer":
+            return "208"
+        else:
+            print("ERROR: check manufacturer column (cvx)")
+            print(manu)
+            return False
+
+    def get_reaction(self):
+        return self._reaction_code
+
+    def get_ndc(self):
+        return self._ndc
+
+    def get_trade_name(self):
+        return self._trade_name
+
+    def get_cpt(self):
+        return self._cpt
+
+    def get_cvx(self):
+        return self._cvx
+
+    def get_vaccine_group(self):
+        return self._vaccine_group
+
+    def get_vaccination_date(self):
+        return format_date(self._vaccination_date)
+
+    def get_route(self):
+        return self._route_code
+
+    def get_body_site(self):
+        return format_body_site(self._body_site)
+
+    def get_manufacturer_code(self):
+        return self._manufacturer_code
+
+    def get_info_source(self):
+        return self._info_source
+
+    def get_lot_number(self):
+        return self._lot_number
+
+    def get_provider_name(self):
+        return self._provider_name
+
+    def get_giver(self):
+        return self._giver
+
+    def get_sending_org(self):
+        return self._sending_org
+
+    def get_eligibility(self):
+        return self._eligibility
